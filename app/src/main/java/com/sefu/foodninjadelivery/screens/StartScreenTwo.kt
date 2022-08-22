991db41af3c2784b42b62bc9152350e1b822d7fe
Templa-io <humphreywritter@gmail.com>
2022-08-22 11:53:16 +0300

    Commit create StartScreenTwo

--- app/src/main/java/com/sefu/foodninjadelivery/screens/StartScreenTwo.kt
+++ app/src/main/java/com/sefu/foodninjadelivery/screens/StartScreenTwo.kt
@@ -1,19 +1,66 @@
 package com.sefu.foodninjadelivery.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sefu.foodninjadelivery.R
 import com.sefu.foodninjadelivery.ui.theme.FoodNinjaDeliveryTheme
 
 @Composable
 fun StartScreenTwo(modifier: Modifier = Modifier) {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.White)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Column(modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.illustartion),
+                contentDescription = "Illustration",
+                modifier = Modifier.size(400.dp)
+            )
+            Box() {
+                Column(modifier = Modifier.fillMaxWidth().padding(horizontal =50.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(28.dp)) {
+                    Text(
+                        text = "Find your Comfort\nFood Here",
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Here You can find a chef or dish for every\ntaste and color. Enjoy!",
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(12.dp))
+                    .size(width = 150.dp, height = 50.dp)
+                    .background(color = Color.Green),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Next", fontSize = 20.sp, style = TextStyle(color = Color.White))
+            }
+        }
 
     }
 }

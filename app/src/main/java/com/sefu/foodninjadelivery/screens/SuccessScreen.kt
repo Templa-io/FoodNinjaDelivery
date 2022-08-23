e2013698057839fff4a5cc3f1c2b0b98de313f9a
Templa-io <humphreywritter@gmail.com>
2022-08-23 20:06:38 +0300

    Initial Commit create SuccessScreen

---
+++ app/src/main/java/com/sefu/foodninjadelivery/screens/SuccessScreen.kt
@@ -0,0 +1,18 @@
+package com.sefu.foodninjadelivery.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.sefu.foodninjadelivery.ui.theme.FoodNinjaDeliveryTheme
+
+@Composable
+fun SuccessScreen() {
+
+}
+
+@Preview
+@Composable
+fun PreviewSuccessScreen() {
+    FoodNinjaDeliveryTheme {
+        SuccessScreen()
+    }
+}

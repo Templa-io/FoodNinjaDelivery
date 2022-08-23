dcf51c59dc8e76b9bd28fcc6c1bbab5f07082d83
Templa-io <humphreywritter@gmail.com>
2022-08-23 21:04:20 +0300

    Commit create VerifyScreen

--- app/src/main/java/com/sefu/foodninjadelivery/screens/VerifyScreen.kt
+++ app/src/main/java/com/sefu/foodninjadelivery/screens/VerifyScreen.kt
@@ -1,4 +1,112 @@
 package com.sefu.foodninjadelivery.screens
 
-class VerifyScreen {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowLeft
+import androidx.compose.material.icons.rounded.LocationOn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sefu.foodninjadelivery.R
+import com.sefu.foodninjadelivery.ui.theme.FoodNinjaDeliveryTheme
+
+@Composable
+fun VerifyScreen(modifier: Modifier=Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box() {
+                Column(verticalArrangement = Arrangement.spacedBy(24.dp)) {
+                    Box(
+                        modifier = Modifier
+                            .clip(shape = RoundedCornerShape(12.dp))
+                            .background(colorResource(id = R.color.Light_baige).copy(0.1f))
+                            .size(60.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            Icons.Rounded.KeyboardArrowLeft,
+                            contentDescription = "Arrow",
+                            modifier = Modifier.size(50.dp),
+                            tint = colorResource(id = R.color.baige)
+                        )
+                    }
+                    Box() {
+                        Text(
+                            text = "Enter 4-digit\nVerification code",
+                            fontSize = 32.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    Text(text = "Code send to +2542045**** this code will\nexpire in 01:30")
+                    Card(elevation = 10.dp, shape = RoundedCornerShape(12.dp)) {
+                        Column(modifier = Modifier.padding(28.dp)) {
+
+
+                                    Spacer(modifier = Modifier.width(12.dp))
+                                    Text(text = "1     9     2     3", fontSize = 50.sp)
+
+
+
+
+
+                        }
+                    }
+
+                }
+            }
+            Box(
+                contentAlignment = Alignment.Center, modifier = Modifier
+                    .clip(shape = RoundedCornerShape(16.dp))
+                    .background(
+                        brush = Brush.linearGradient(
+                            colors = listOf(
+                                colorResource(id = R.color.light_green),
+                                colorResource(id = R.color.dark_green)
+                            )
+                        )
+                    )
+                    .size(width = 150.dp, height = 50.dp)
+            ) {
+                Text(
+                    text = "Next",
+                    fontSize = 20.sp,
+                    style = TextStyle(color = Color.White)
+                )
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewVerifyScreen() {
+    FoodNinjaDeliveryTheme {
+        VerifyScreen()
+    }
 }
